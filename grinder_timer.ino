541cc0dec4db4bf5fd4c42aa85530973fc37aaca
Jakub Kohoutek <jkohoutek@microsoft.com>
2021-10-19 09:43:45 +0200

    Implement interactive menu and make timer work

--- grinder_timer.ino
+++ grinder_timer.ino
@@ -6,12 +6,18 @@
 #include <U8g2lib.h>
 #include <Encoder.h>
 
+#include "memory.h"
+
 #define ENCODER_A       2   // Must be an interrupt pin for sufficient reliability
 #define ENCODER_B       3   // Must be an interrupt pin for sufficient reliability
 #define ENCODER_BUTTON  4
+#define LONG_PRESS_MS   2000
 #define ENCODER_STEP    3
 #define BUS_CLOCK_SPEED 400000
 #define RELAY_PIN       7
+
+#define SINGLE_DOSE_ADDRESS  0
+#define DOUBLE_DOSE_ADDRESS  4
 
 // Display settings with buffer
 // U8G2_SH1106_128X64_NONAME_1_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
@@ -27,16 +33,36 @@ U8X8_SH1106_128X64_NONAME_HW_I2C  display(U8X8_PIN_NONE);
 Encoder                           encoder(ENCODER_A, ENCODER_B);
 Button                            pushButton(ENCODER_BUTTON);
 
-long encoderPosition = 0;
-unsigned long oneDoseTimeout = 0;
+enum MenuItem {
+  SingleDose,
+  DoubleDose,
+  Settings,
+  Statistics
+};
+
+enum Screen {
+  MenuScreen,
+  SettingsScreen,
+  Timer,
+  StatisticsScreen
+};
+
+long           encoderPosition = 0;
+unsigned long  oneDoseTimeout  = 0;
+unsigned long  twoDoseTimeout  = 0;
+Screen         currentScreen   = MenuScreen;
+MenuItem       menu[]         = {SingleDose, DoubleDose, Statistics};
+MenuItem*       selectedItem    = menu;
+short          menuLength      = sizeof(menu)/sizeof(*menu);
+bool           ignoreNextPush  = false;
 
 void drawMessage (const char* message, bool inverse = false, uint8_t row = 0) {
   if(inverse)
     display.inverse();
   display.setFont(u8x8_font_8x13B_1x2_r);
   uint8_t line = row * 2;
-  display.clearLine(line);
-  display.clearLine(line + 1);
+//  display.clearLine(line);
+//  display.clearLine(line + 1);
   display.drawString(0, line, message);
   if(inverse)
     display.noInverse();
@@ -49,6 +75,28 @@ void showTime (unsigned long milliseconds) {
   String padding = seconds < 10 ? " " : "";
   display.setFont(u8x8_font_inr21_2x4_n);
   display.drawString(3, 3, (padding + String(seconds) + ":" + String(secondDecimals)).c_str());
+}
+
+const char * getMenuItemString (MenuItem menuItem) {
+  switch(menuItem) {
+    case SingleDose:
+      return "Single dose";
+    case DoubleDose:
+      return "Double dose";
+    case Settings:
+      return "Settings";
+    case Statistics:
+      return "Statistics";
+  }
+}
+
+void showMenu () {
+  currentScreen = MenuScreen;
+  for (short i; i < menuLength; i++) {
+    String selectionIndicator = *selectedItem == menu[i] ? "> " : "  ";
+    String lineText = selectionIndicator + getMenuItemString(menu[i]);
+    drawMessage(lineText.c_str(), false, i);
+  }
 }
 
 void setup () {
@@ -64,58 +112,166 @@ void setup () {
   display.drawString(1, 4, " be with you! ");
   delay(2000);
 
-
-  drawMessage("Jedna", false, 0);
-  delay(500);
-  drawMessage("Dva", false, 1);
-  delay(500);
-  drawMessage("Tri", false, 2);
-  delay(500);
-  drawMessage("Ctyri", false, 3);
-  delay(500);
   display.clearDisplay();
+//  writeToMemory(SINGLE_DOSE_ADDRESS, 0);
+//  writeToMemory(DOUBLE_DOSE_ADDRESS, 0);
+  oneDoseTimeout = readFromMemory(SINGLE_DOSE_ADDRESS);
+  twoDoseTimeout = readFromMemory(DOUBLE_DOSE_ADDRESS);
+  
+  showMenu();
+}
+
+void changeTimeout(unsigned long * timeoutPtr, bool isIncrease) {
+  if (isIncrease){
+    *timeoutPtr += 100;
+  } else if (*timeoutPtr >=100) {
+    *timeoutPtr -= 100;
+  }
+  showTime(*timeoutPtr);
+}
+
+void handleRotation (bool clockwise) {
+  switch(currentScreen) {
+    case MenuScreen:
+      if (clockwise){
+        if (*selectedItem == menu[menuLength - 1]) {
+           selectedItem = menu;
+        } else {
+           selectedItem++;
+        }
+      } else {
+        if (*selectedItem == menu[0]) {
+           selectedItem = &menu[menuLength - 1];
+        } else {
+           selectedItem--;
+        }
+      }
+      showMenu();
+      break;
+    case SettingsScreen:
+      changeTimeout(
+        (*selectedItem == SingleDose) ? &oneDoseTimeout : &twoDoseTimeout,
+        clockwise
+      );
+      break;
+  }
+}
+
+void handleButtonPress () {
+  switch(currentScreen) {
+    case MenuScreen:
+      String loadingBar = "";
+      unsigned long timeSincePress = millis() - pushButton.lastChange();
+      for (int i = 0; i < (timeSincePress / (LONG_PRESS_MS - 300)) * display.getCols(); i++) {
+        loadingBar += " ";
+        drawMessage(loadingBar.c_str(), true, 3);
+      }
+  }
+}
+
+void handleButtonPush () {
+  if (ignoreNextPush) {
+    ignoreNextPush = false;
+    return;
+  }
+
+  switch(currentScreen) {
+    case MenuScreen:
+      if (*selectedItem == SingleDose || *selectedItem == DoubleDose){
+        display.clear();
+        runTimer(*selectedItem);
+        break;
+      }
+      if (*selectedItem == Statistics) {
+        // display.clear();
+        // showStatistics();
+        break;
+      }
+    case SettingsScreen:
+      bool singleDose = *selectedItem == SingleDose;
+      writeToMemory(
+        singleDose ? SINGLE_DOSE_ADDRESS : DOUBLE_DOSE_ADDRESS,
+        singleDose ? oneDoseTimeout : twoDoseTimeout
+      );
+      display.clear();
+      showMenu();
+      break;
+  }
+}
+
+void handleLongButtonPush () {
+  switch(currentScreen) {
+    case MenuScreen:
+      if (*selectedItem == SingleDose || *selectedItem == DoubleDose){
+          currentScreen = SettingsScreen;
+          display.clear();
+          drawMessage(getMenuItemString(*selectedItem));
+          showTime(*selectedItem == SingleDose ? oneDoseTimeout : twoDoseTimeout);
+        break;
+      }
+      if (*selectedItem == Statistics) {
+        // Reset?
+        break;
+      }
+    default:
+      return;
+  }
+  ignoreNextPush = true;
 }
 
 void loop() {
-    long newPosition = encoder.read();
+  long newPosition = encoder.read();
+  if (newPosition - encoderPosition > ENCODER_STEP) {
+    encoderPosition = newPosition;
+    handleRotation(false);
+  }
 
-    if (newPosition - encoderPosition > ENCODER_STEP) {
-      encoderPosition = newPosition;
-      if (oneDoseTimeout >=100)
-        oneDoseTimeout -= 100;
-      showTime(oneDoseTimeout);
-    }
+  if (newPosition - encoderPosition < -ENCODER_STEP) {
+    encoderPosition = newPosition;
+    handleRotation(true);
+  }
 
-    if (newPosition - encoderPosition < -ENCODER_STEP) {
-      encoderPosition = newPosition;
-      oneDoseTimeout += 100;
-      showTime(oneDoseTimeout);
-    }
-
-    pushButton.read();
-    if (pushButton.wasPressed()) {
-      runTimer();
-    }
+  pushButton.read();
+  if (pushButton.isPressed()) {
+    handleButtonPress();
+  }
+  if (pushButton.wasReleased()) {
+    handleButtonPush();
+  } else if (pushButton.pressedFor(LONG_PRESS_MS)) {
+    handleLongButtonPush();
+  }
 }
 
-
-void runTimer () {
+void runTimer (MenuItem selectedItem) {
+  currentScreen = Timer;
+  display.clear();
   drawMessage("Grinding...");
   unsigned long startTime = millis();
   unsigned long millisecondsFromStart = 0;
+  bool stopped = false;
 
   digitalWrite(RELAY_PIN, LOW);
 
-  while (millisecondsFromStart <= oneDoseTimeout) {
+  unsigned long timeout = selectedItem == SingleDose ? oneDoseTimeout : twoDoseTimeout;
+
+  while (millisecondsFromStart <= timeout) {
     pushButton.read();
-    if (pushButton.wasPressed()) {
-      drawMessage("Timer stopped", true);
+    if (pushButton.wasReleased()) {
+      stopped = true;
       break;
     }
 
     millisecondsFromStart = millis() - startTime;
     showTime(millisecondsFromStart);
   }
-
   digitalWrite(RELAY_PIN, HIGH);
+
+  if (stopped) {
+    drawMessage("Timer stopped", true);
+  } else {
+    drawMessage("Done, enjoy!");
+  }
+  delay(2000);
+  display.clear();
+  showMenu();
 }
